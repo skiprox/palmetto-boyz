dff98c600d9268515aeaf2f1ad8c9511a3c978d5
Sean Scanlan <sean.scanlan@kettlenyc.com>
2015-01-30 14:10:08 -0500

    Adding socket.io stuff

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+  	console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg) {
+  	console.log('message: ' + msg);
+  	io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
