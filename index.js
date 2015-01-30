a61fd4bc8652435cee8676c67ff333da02327ed3
Sean Scanlan <sean.scanlan@kettlenyc.com>
2015-01-30 15:20:10 -0500

    Updating, trying again

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'));
-
-app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -21,6 +20,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(app.get('port'), function(){
   console.log('listening on:' + app.get('port'));
 });
