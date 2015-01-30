1b90818914e614b5d4d905d4f1594525d200e4ed
Sean Scanlan <sean.scanlan@kettlenyc.com>
2015-01-30 15:12:58 -0500

    Adding socket stuff back in, fixing it

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function() {
-//   	console.log('user disconnected');
-//   });
-//   socket.on('chat message', function(msg) {
-//   	io.emit('chat message', msg);
-//   });
-// });
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+  	console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg) {
+  	io.emit('chat message', msg);
+  });
+});
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on:' + app.get('port'));
 });
