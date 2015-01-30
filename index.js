6b77d6074633edb044a0f52bf6bf066280c7d354
Sean Scanlan <sean.scanlan@kettlenyc.com>
2015-01-30 15:03:05 -0500

    getting rid of socket stuff to see if this works on heroku

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-  	console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg) {
-  	io.emit('chat message', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function() {
+//   	console.log('user disconnected');
+//   });
+//   socket.on('chat message', function(msg) {
+//   	io.emit('chat message', msg);
+//   });
+// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
