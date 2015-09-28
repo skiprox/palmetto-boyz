ca87da5d245aa8b35c2678084f834354d01b05cb
Sean Scanlan <sean.scanlan@kettlenyc.com>
2015-09-28 17:56:45 -0400

    Updating app to comment out stuff

--- public/app.js
+++ public/app.js
@@ -11,45 +11,21 @@ Public = (function(){
 
 	var _addDomListeners = function() {
 		$('form').submit(function() {
-			socket.emit('chat message', $('#message').val());
+			socket.emit('new message', $('#message').val());
 			$('#message').val('');
 			return false;
 		});
 	};
 
 	var _addSocketListeners = function() {
-		socket.on('chat message', function(msg) {
-			_printMessage(msg);
-			var msgArray = msg.split('.'),
-				msgEnding = msgArray[msgArray.length - 1];
-			if (msgEnding === 'jpg' || msgEnding === 'jpeg' || msgEnding === 'png' || msgEnding === 'gif') {
-				$('#messages').append($('<li><img src="' + msg + '"></li>'));
-			}
-			else if (msg.match(/\barnold\b/) !== null) {
-				$('#messages').append($('<li class="arnold">').text(runningMan.quote()));
-			}
-			else if (msg.match(/\bface\b/) !== null) {
-				$('#messages').append($('<li class="face">').text(faces()));
-			}
-			$('html, body').animate({scrollTop: $('body').height()}, 200);
-		});
-
-		socket.on('user joined', function(obj) {
-			console.log(obj);
-			if (obj.didJoin) {
-				$('#messages').append($('<li class="room-activity">').text('[A user has joined (' + obj.users + ' in room)]'));
-			}
-			else {
-				$('#messages').append($('<li class="room-activity">').text('[A user has left (' + obj.users + ' in room)]'));
-			}
+		socket.on('new message', function(data) {
+			addChatMessage(data);
 		});
 	};
 
-	var _printMessage = function(msg) {
-		if (msg !== '') {
-			$('#messages').append($('<li>').text(msg));
-		}
-	}
+	var addChatMessage = function(data) {
+
+	};
 
 	return {
 		initialize : function() {
