d8311791aab757b88b61ec438320ba7739769771
skiprox <seanmichaelscanlan@gmail.com>
2015-10-14 12:18:45 -0700

    Working on adding new chat rooms

--- public/app.js
+++ public/app.js
@@ -77,15 +77,32 @@ Public = (function(){
 		});
 		UI.sidebarPeople.addEventListener('click', function(e) {
 			e.preventDefault();
-			socket.emit('start private chat', e.target.getAttribute('data-id'));
-			// Need to add real data from user who clicked here
-			var data = {
-				username: "paul",
-				usernumber: 1,
-				userId: e.target.getAttribute('data-id')
+			var userId = e.target.getAttribute('data-id');
+			// Check if the chatroom defined by the userId exists,
+			// if not we create it and initiate private chat
+			if (!UI[userId]) {
+				// Create the chat room
+				UI[userId] = {};
+				UI[userId].chatRoom = UI.main.chatRoom.cloneNode(true);
+				UI[userId].chatRoom.setAttribute('id', 'room-' + userId);
+				UI[userId].chatForm = UI[userId].chatRoom.querySelector('.chat-form');
+				UI[userId].chatInput = UI[userId].chatRoom.querySelector('.chat-form__input');
+				UI[userId].messagesList = UI[userId].chatRoom.querySelector('.message-list');
+				UI[userId].messagesList.innerHTML = '';
+				UI.body.appendChild(UI[userId].chatRoom);
+				socket.emit('start private chat', e.target.getAttribute('data-id'));
+				// Need to add real data from user who clicked here
+				var data = {
+					username: e.target.textContent,
+					usernumber: parseInt(e.target.getAttribute('data-usernumber'), 10),
+					userId: e.target.getAttribute('data-id')
+				}
+				openChatRoomWith(data);
+				return false;
 			}
-			openChatRoomWith(data);
-			return false;
+			else {
+				return false;
+			}
 		});
 	};
 
@@ -109,7 +126,7 @@ Public = (function(){
 
 	/**
 	 * When someone leaves or enters the group, this gets called
-	 * @param  {Object} -- includes data.username, data.usernumber, and data.userCount
+	 * @param  {Object} -- includes data.username, data.usernames, data.usernumber, data.userIds, and data.userCount
 	 * @param  {String} -- either "joined" or "left", from _addSocketListeners()
 	 */
 	var updateGroupUsers = function(data, infoString) {
@@ -117,8 +134,10 @@ Public = (function(){
 		UI.main.messagesList.innerHTML += '<li class="message notification notification--total-number">There are now ' + data.userCount + ' users in the group.</li>';
 		forceScrollToBottom();
 		UI.sidebarPeople.innerHTML = '';
+		var i = 0;
 		for (var name in data.usernames) {
-			UI.sidebarPeople.innerHTML += '<li id="person-' + data.usernames[name] + '" data-id="' + data.userIds[name] + '" class="person">' + data.usernames[name] + '</li>';
+			UI.sidebarPeople.innerHTML += '<li id="person-' + data.usernames[name] + '" data-id="' + data.userIds[name] + '" data-usernumber="' + i + '" class="person">' + data.usernames[name] + '</li>';
+			i++;
 		}
 	};
 
